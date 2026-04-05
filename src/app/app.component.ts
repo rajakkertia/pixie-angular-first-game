23ef04e7b34fe425fe27e8377bea9073a70e03bb
rajakkertia <sandro.baramidze@gmail.com>
2026-04-05 23:38:16 +0400

    unused code removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { PixiCanvasComponent } from './pixi-canvas/pixi-canvas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PixiCanvasComponent],
+  imports: [CommonModule, PixiCanvasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
